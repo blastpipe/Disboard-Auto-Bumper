3b115f808ca8447ec96710c55d3ec895eff3cb0a
blastpipe <88486744+blastpipe@users.noreply.github.com>
2022-05-23 01:29:11 -0500

    Update 🌌bumper.py

--- 🌌bumper.py
+++ 🌌bumper.py
@@ -1,93 +1,59 @@
-import discord, os, time
-from discord.ext import commands # https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html
-
-# More improvements examples: 
-# Advanced features: https://codeberg.org/SnowCode/auto-bump
-
-
-# Uncomment the line below if you are wanting to host this on heroku and are using an environment variable to store the token.
-token = os.getenv("USER_TOKEN")
-# If you are using this on a server or your home pc uncomment the line below and put the discord token for the account you want it to auto bump on.
-#token = "TOKEN"
-
-botReceive = commands.Bot(command_prefix = "--", self_bot=True) # https://discordpy.readthedocs.io/en/stable/ext/commands/api.html?highlight=run#bot
-
-@botReceive.event
-async def on_ready():
-   
-    print("Discord.py Version: " + discord.__version__)
-    print("Auto Bumper Is Online!") 
-    print("USER_TOKEN: " + os.getenv("USER_TOKEN"))
-   
-    # https://discordpy.readthedocs.io/en/stable/ext/commands/api.html?highlight=run#discord.ext.commands.Bot.user
-    print("Auto Bumper with Username: " + str(botReceive.user) + " | User ID: " + str(botReceive.user.id)) 
-  
-
-    print("Channels are available from these joined Discord Servers: ")
-    for guild in botReceive.guilds:
-        print("  [!] Server Name: " + str(guild.name) + " | Server ID: " + str(guild.id))
-
-    # Initializing channel where to send !d bump
-    # Example: fetch_channel("855039765711552515")
-    print ("DISCORD_SERVER_CHANNEL_ID: " + str(os.getenv("DISCORD_SERVER_CHANNEL_ID")))
-    print ("DISCORD_SERVER_CHANNEL_NAME: #" + str(await botReceive.fetch_channel(os.getenv("DISCORD_SERVER_CHANNEL_ID"))))
-    channel = await botReceive.fetch_channel(os.getenv("DISCORD_SERVER_CHANNEL_ID"));
-   
-    # Send --bla command automatically on the bot initialization.
-    # await channel.send("--bla")
-   
-    # Some Loop Testing
-    while True:
-        await channel.send("found channel")
-        await channel.send("/bump ")
-        time.sleep(8125) # 8125 seconds == 2 hours
-        
-        
-        
-    #server = bot.get_server(member.server)
-    #channel = discord.utils.get(server.text_channels, name='general')
-    
-    #print(str(botReceive.user.fetch_channel))
-    
-    #https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20utils%20get#discord.utils.get
-    # print(str(discord.utils.get(async botReceive.fetch_guild.text_channels, name="general")))
-    #print(botReceive.get_channel('855039765711552515'))
-
-@botReceive.command()
-async def bla(ctx): # https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#discord.ext.commands.Context
-    while True:
-      await ctx.send("!d bump")
-      await ctx.send("Username: " + str(ctx.author))
-      await ctx.send("Channel ID: " + str(ctx.channel.id))
-      print("Auto Bumper Channel ID: " + str(ctx.channel.id))
-      print("Auto Bumper bla command has been done in #" + str(ctx.channel) + " by " + str(ctx.author))
-      time.sleep(10) # 8125 seconds == 2 hours
-
-
-
-botReceive.run(token, bot = False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@botReceive.event
-async def on_connect():
-    print("Auto Bumper successfully connected to Discord!")
-
-
-    
-@botReceive.event
-async def on_disconnect():
-    print("Auto Bumper: Discord.com is down or the Bot can not connect to the internet")
+import discord, datetime
+from asyncio import sleep
+from config import *
+class Client(discord.Client):
+    active = True
+    async def pauseBump(self):
+        self.active = False
+        print("Bump paused.")
+    async def clean(self, user_message=None, bot_message=None):
+        await sleep(3)
+        if user_message is not None:
+            await user_message.delete()
+        if bot_message is not None:
+            await bot_message.delete()
+        print("Chat cleaned")
+    async def continueBump(self):
+        self.active = True
+        print("Bump restored.")
+    async def send(self, ctx, content):
+        message = await ctx.channel.send(f"{ctx.author.mention} {content}")
+        print(f"Sent '{content}' to '{ctx.author}'")
+        await self.clean(ctx, message)
+    async def bumpCheck(self):
+        channel = self.get_channel(channel_id)
+        async for message in channel.history(limit=50):
+            if str(message.author) == "DISBOARD#2760":
+                if "Bump done" in message.embeds[0].description:
+                    now = datetime.datetime.utcnow()
+                    two = datetime.timedelta(hours=2)
+                    min = datetime.timedelta(minutes=1)
+                    difference = now - message.created_at
+                    difference = two - difference + min
+                    print(f"Time until next bump {difference}")
+                    return difference.seconds
+                    break
+    async def bump(self):
+        self.diff = await self.bumpCheck()
+        await sleep(self.diff)
+        channel = self.get_channel(channel_id)
+        command = await channel.send("/bump ")
+        print("Server bumped")
+        return command
+    async def on_ready(self):
+        print(f"Logged as {self.user}")
+        while self.active == True:
+            command = await self.bump()
+            await self.clean(command)
+    async def on_message(self, message):
+        if message.author == self.user:
+            if message.content == "!pause":
+                await self.pauseBump()
+                await self.send(message, "Bot is paused :sleeping:")
+            elif message.content == "!continue":
+                await self.continueBump()
+                await self.send(
+                    message,
+                    f"Bump is activated, next bump in {self.diff} seconds :hourglass_flowing_sand:",
+                )
+Client().run("OTY3OTQ5MjYwNDU0NzY4Njkw.G1oakg.4qZuFwZ1PBAgJ_fOkwDxjryfpKCsvz29aAimjo", bot=False)
